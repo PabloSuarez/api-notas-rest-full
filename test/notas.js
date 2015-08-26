dbbcfa6239a60ac0fa0ab4b88ea0dd04644d5f43
Pablo Suarez <hostsuarezpablo@gmail.com>
2015-08-26 04:01:12 -0300

    implemento PUT

--- test/notas.js
+++ test/notas.js
@@ -1,7 +1,8 @@
 var request = require('supertest-as-promised'),
     api = require('../server.js'),
     async = require('async'),
-    host = process.env.API_TEST_HOST || api
+    host = process.env.API_TEST_HOST || api,
+    logger = require('../lib/logger')
 
 request = request(host)
 
@@ -34,6 +35,7 @@ function assertions(res, data, callback) {
   expect(nota).to.have.property('type', data.nota.type)
   expect(nota).to.have.property('body', data.nota.body)
   expect(nota).to.have.property('id', data.nota.id)
+  callback(null, nota)
 }
 
 describe('Coleccion de Notas [/notas]', function() {
@@ -55,12 +57,10 @@ describe('Coleccion de Notas [/notas]', function() {
         },
         function (res, callback) {
           assertions(res, data, callback)
-          done()
         }
       ],done)
     })
   })
-
 
   describe('GET obtengo una NOTA', function () {
     it('deberia obtener una nota existente', function (done) {
@@ -83,6 +83,55 @@ describe('Coleccion de Notas [/notas]', function() {
         },
         function (res, callback){
           assertions(res, data, callback)
+        }
+      ], done)
+    })    
+  })
+
+  describe('PUT Modificar una nota', function () {
+    it('deberia obtener una nota y modificarla', function (done) {
+      var data = {
+        "nota": {
+          "title": "MI NOTA #node #node-pro",
+          "description": "Introduccion a clase",
+          "type": "js",
+          "body": "soy el cuerpo de json"
+        }
+      }
+
+      var newData = {
+        "nota": {
+          "title": "MI NOTA 'Actualizada'",
+          "description": "Introduccion a clase",
+          "type": "js",
+          "body": "soy el cuerpo de json"
+        }
+      }
+      var id
+
+      async.waterfall([
+        function (callback) {
+          createNotes(data, callback)
+        },
+        function (res, callback) {
+          getNote(res, callback)
+        },
+        function (res, callback){
+          assertions(res, data, callback)
+        },
+        function (res, callback) {
+          // Ya cree la nota, ahora la actualizo
+          newData.nota.id = res.id
+          request
+            .put('/notas/' + res.id)
+            .set('Accept', 'application/json')
+            .send(newData)
+            .expect(200)
+            .end(callback)
+        },
+        function (res, callback) {
+          // assertions(res, data, callback)
+          console.log('FINAL')
           done()
         }
       ], done)
