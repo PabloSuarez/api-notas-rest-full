d9701ea6a90db1aea6a42c6ea28a16edb5463f2b
Pablo Suarez <hostsuarezpablo@gmail.com>
2015-08-26 00:49:04 -0300

    usando promesas

--- test/notas.js
+++ test/notas.js
@@ -1,88 +1,91 @@
-var request = require('supertest-as-promised')
-var api = require('../server.js')
-var host = process.env.API_TEST_HOST || api
+var request = require('supertest-as-promised'),
+    api = require('../server.js'),
+    async = require('async'),
+    host = process.env.API_TEST_HOST || api
 
 request = request(host)
 
+
+function createNotes(data, callback) {
+  // crear nota nueva
+  request
+    .post('/notas/')
+    .set('Accept', 'application/json')
+    .send(data)
+    .expect(201)
+    .end(callback)
+}
+
+function getNote(res, callback) {
+  id = res.body.nota.id
+
+  request
+    .get('/notas/' + id)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .end(callback)
+}
+
+function assertions(res, data, callback) {
+  var nota = res.body.notas
+
+  expect(nota).to.have.property('title', data.title)
+  expect(nota).to.have.property('description', data.description)
+  expect(nota).to.have.property('type', data.type)
+  expect(nota).to.have.property('body', data.body)
+  expect(nota).to.have.property('id', data.id)
+}
+
 describe('Coleccion de Notas [/notas]', function() {
 
-  describe('POST', function() {
+  describe('POST creo una nota', function() {
     it('deberia crear una nota', function(done) {
       var data = {
         "nota": {
-          "title": "Mejorando.la #node-pro",
+          "title": "Creo MI NOTA #node #node-pro",
           "description": "Introduccion a clase",
           "type": "js",
           "body": "soy el cuerpo de json"
         }
       }
 
-      request
-        .post('/notas')
-        .set('Accept', 'application/json')
-        .send(data)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-        .end(function(err, res) {
-          var nota
-
-          var body = res.body
-          console.log('body', body)
-
-          // Nota existe
-          expect(body).to.have.property('nota')
-          nota = body.nota
-
-          // Propiedades
-          expect(nota).to.have.property('title', 'Mejorando.la #node-pro')
-          expect(nota).to.have.property('description', 'Introduccion a clase')
-          expect(nota).to.have.property('type', 'js')
-          expect(nota).to.have.property('body', 'soy el cuerpo de json')
-          expect(nota).to.have.property('id')
-
-          done(err)
-        })
+      async.waterfall([
+        function (callback) {
+          createNotes(data, callback)
+        },
+        function (res, callback) {
+          assertions(res, data, callback)
+          done()
+        }
+      ],done)
     })
   })
 
 
-  describe('GET ', function () {
+  describe('GET obtengo una NOTA', function () {
     it('deberia obtener una nota existente', function (done) {
       var data = {
         "nota": {
-          "title": "Mejorando.la #node-pro",
+          "title": "MI NOTA #node #node-pro",
           "description": "Introduccion a clase",
           "type": "js",
           "body": "soy el cuerpo de json"
         }
       }
-      var id = 0;
+      var id
 
-      // crear nota nueva
-      request
-        .post('/notas/')
-        .set('Accept', 'application/json')
-        .send(data)
-        .expect(201)
-        .then(function (res) {
-          id = res.body.nota.id
-          return request
-            .get('/notas/'+id)
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-        }, done)
-
-        .then(function (res){
-          var nota = res.body.notas
-
-          // Propiedades
-          expect(nota).to.have.property('title', 'Mejorando.la #node-pro')
-          expect(nota).to.have.property('description', 'Introduccion a clase')
-          expect(nota).to.have.property('type', 'js')
-          expect(nota).to.have.property('body', 'soy el cuerpo de json')
-          expect(nota).to.have.property('id', id)
-        }, done())
-
+      async.waterfall([
+        function (callback) {
+          createNotes(data, callback)
+        },
+        function (res, callback) {
+          getNote(res, callback)
+        },
+        function (res, callback){
+          assertions(res, data, callback)
+          done()
+        }
+      ], done)
     })
   })
 
